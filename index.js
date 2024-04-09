2b348537304203a583ae3ab4426cce806b4d9124
Sundanpatyad <sundansharma600@gmail.com>
2024-04-09 23:40:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import  Connection  from './database/db.js';
+import route from './routes/routes.js';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+const app = express();
+app.use(bodyParser.json({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
+app.use('/', route)
+Connection();
+
+
+app.get('/', (req, res) => {
+    res.send('hello');
+  });
+const PORT = 8000;
+
+app.listen(PORT , ()=> console.log("Server Is Running", PORT))
